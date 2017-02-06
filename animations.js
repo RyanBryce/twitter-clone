24e6f0909ce1cbe29d96398402b13ca29630e22a
Ryan Bryce <ryanbryce@Ryans-MacBook-Pro.local>
2017-02-05 20:39:44 -0700

    blackdimond

--- animations.js
+++ animations.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
   $('#tweet-controls').addClass('tweet-controls-hidden')
-    $('.tweet-compose').on('click', function() {
-        $(this).addClass('tweet-compose-dubH');
-        $('#tweet-controls').removeClass('tweet-controls-hidden');
+  $('.tweet-compose').on('click', function() {
+    $(this).addClass('tweet-compose-dubH');
+    $('#tweet-controls').removeClass('tweet-controls-hidden');
     })
-    $('#tweet-submit').attr('disabled', 'disabled'); //static button kaputt
-    $('#area').keyup(function() {
-        var chars = $(this).val().length; //gets length of texarea
-
-        $('#char-count').text(140 - chars);
-        if (chars >= 130) { //sets text warning perams
+  $('#tweet-submit').attr('disabled', 'disabled'); //static button kaputt
+  $('#area').keyup(function() {
+    var chars = $(this).val().length; //gets length of texare
+    $('#char-count').text(140 - chars);
+          if (chars >= 130) { //sets text warning perams
             $('#char-count').attr('style', 'color:red');
         } else {
             $('#char-count').attr('style', 'color:#999');
@@ -21,10 +20,9 @@ $(document).ready(function() {
             $('#tweet-submit').removeAttr('disabled');
         };
     });
-    $('#tweet-submit').on('click', function() {
-        $('#tweet-controls').addClass('tweet-controls-hidden');
-        $('.tweet-compose').removeClass('tweet-compose-dubH');
-        var message = $('#area').val();
+  $('#tweet-submit').on('click', function() {
+      $('#tweet-controls').addClass('tweet-controls-hidden');        $('.tweet-compose').removeClass('tweet-compose-dubH');
+      var message = $('#area').val();
         $('#stream').prepend(
             '<div class="tweet">' +
               '<div class="content">' +
@@ -32,21 +30,21 @@ $(document).ready(function() {
                 '<strong class="fullname">Crazy Ted</strong>' +
                 '<span class="username">@mCrazyTed</span>' +
                 '<p class="tweet-text">' + message + '</p>' +
-              '<div class="tweet-actions">' +
-              '<ul>' +
-                '<li>' + '<span class="icon action-reply">' + '</span>' + " " + 'Reply' + " " + '</li>' +
-                '<li>' + '<span class="icon action-retweet">' + '</span>' + " " + 'Retweet' + " " + '</li>' +
-                '<li>' + '<span class="icon action-favorite">' + '</span>' + " " + 'Favorite' + " " + '</li>' +
-                '<li>' + '<span class="icon action-more">' + '</span>' + " " + 'More' + " " + '</li>' +
-                '</ul>' +
-            '</div>' +
+                '<div class="tweet-actions">' +
+                  '<ul>' +
+                  '<li>' + '<span class="icon action-reply">' + '</span>' + " " + 'Reply' + " " + '</li>' +
+                  '<li>' + '<span class="icon action-retweet">' + '</span>' + " " + 'Retweet' + " " + '</li>' +
+                  '<li>' + '<span class="icon action-favorite">' + '</span>' + " " + 'Favorite' + " " + '</li>' +
+                  '<li>' + '<span class="icon action-more">' + '</span>' + " " + 'More' + " " + '</li>' +
+                  '</ul>' +
+              '</div>' +
             '<div class="stats">' +
               '<div class="retweets">' +
                 '<p class="num-retweets">30</p>' +
                 '<p>RETWEETS</p>' +
               '</div>' +
               '<div class="favorites">' +
-                '<p class="num-favorites">6</p>' +
+                '<p class="num-favorites">'+initVal+'</p>' +
                 '<p>FAVORITES</p>' +
               '</div>' +
               '<div class="users-interact">' +
@@ -66,22 +64,32 @@ $(document).ready(function() {
             '</div>' +
             '</div>' +
             '</div>'
-        );
-        $('#area').val('');
-        $('.tweet').click(function(){
+            );
+
+          $('#area').val('');
+          $('.tweet').click(function(){
           $(this).find('.stats').show();
           $(this).find('.reply').show();
         })
-    });
-    $('.tweet').click(function(){
-      $(this).find('.stats').show();
-      $(this).find('.reply').show();
-      $('#tweet-controls').addClass('tweet-controls-hidden');
-      $('.tweet-compose').removeClass('tweet-compose-dubH');
-    })
-    // $('.tweet').mouseout(function(){
-    //   $(this).find('.stats').show();
-    //   $(this).find('.reply').show();
-    // })
+      });
+      $('.tweet').click(function(){
+        $(this).find('.stats').show('slow');
+        $(this).find('.reply').show('slow');
+        $('#tweet-controls').addClass('tweet-controls-hidden');
+        $('.tweet-compose').removeClass('tweet-compose-dubH');
+      })
+      var initVal = 0;
+      $('.num-favorites').text(initVal);
+      $('.action-favorite').click(function() {
+       var numFavoritesElem = $(this)
+                         .closest('.content')
+                         .find('.num-favorites');
 
-});
+      var numFavorites = Number(numFavoritesElem.text());
+      numFavoritesElem.text(numFavorites + 1);
+     });
+     localStorage.setItem('stream', 'Crazy Ted');
+      $('#stream').prepend(localStorage.getItem('stream'))
+
+
+   });
